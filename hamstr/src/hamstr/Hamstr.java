36c6bb4637a0bda235618382c76033f21ae58c84
M <M@M-PC>
2015-11-05 08:16:16 +0200

    Updated sort algorithm. Switched to mergesort and started controlling total number of hamsters in sorting algorithm.

--- hamstr/src/hamstr/Hamstr.java
+++ hamstr/src/hamstr/Hamstr.java
@@ -2,15 +2,13 @@ package hamstr;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Hamstr {
+	private int[][] aux;
+	private int hamstersNumber;
 	
 	public static void main(String[] args) throws IOException {
 		String inputFile = "hamstr.in";
@@ -24,6 +22,7 @@ public class Hamstr {
 		Hamstr hamstr = new Hamstr();
 		hamstr.execute(inputFile, outputFile);
 	}
+
 	
 	public void execute(String inputFile, String outputFile) throws IOException {		
 		// Read from file
@@ -32,7 +31,7 @@ public class Hamstr {
 		BufferedReader bufferReader = new BufferedReader(fileReader);
 		
 		int foodDailyStock = Integer.parseInt(bufferReader.readLine());
-		int hamstersNumber = Integer.parseInt(bufferReader.readLine());
+		hamstersNumber = Integer.parseInt(bufferReader.readLine());
 		
 		// Initialize container which stores all expected food
 		// consumption properties for each hamster
@@ -48,8 +47,9 @@ public class Hamstr {
 		}
 		bufferReader.close();
 		
+		aux = new int[foodContainer.length][2];
 		sort(foodContainer);
-
+		
 		int affordedHamsters = 0;
 		for (int j = 0; j < hamstersNumber; j++) {
 			int totalFoodConsumed = calculateConsumedFood(foodContainer, j);
@@ -66,6 +66,17 @@ public class Hamstr {
 	
 	private int calculateConsumedFood(int[][] foodContainer, int hamsterInProgress) {
 		int totalFoodConsumed = 0;
+		int[] tempArray = new int[hamsterInProgress + 1];
+		for (int i = 0; i <= hamsterInProgress; i++) {
+			tempArray[i] = foodContainer[i][0] + (hamsterInProgress) * foodContainer[i][1];
+		}
+		
+		for (int i = 0; i < tempArray.length - 1; i++) {
+			if (tempArray[i] > tempArray[i + 1]) {
+				swap(foodContainer, i, i + 1);
+			}
+		}
+		
 		for (int i = 0; i <= hamsterInProgress; i++) {
 			totalFoodConsumed += foodContainer[i][0] + (hamsterInProgress) * foodContainer[i][1];
 		}
@@ -80,30 +91,49 @@ public class Hamstr {
 		fileWriter.close();
 	}
 	
+	private void sortLinear(int[][] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if ((array[i][0] + (hamstersNumber * array[i][1])) == (array[i+1][0] + (hamstersNumber * array[i+1][1]))) {
+				if (array[i][1] > array[i + 1][1]) {
+					swap(array, i, i + 1);
+				}
+			}
+		}
+	}
+	
 	private void sort(int[][] array) {
 		sort(array, 0, array.length - 1);
 	}
 	
 	private void sort(int[][] array, int lo, int hi) {
 		if (lo < hi) {
-			int mid = partition(array, lo, hi);
-			sort(array, lo, mid - 1);
+			int mid = (lo + hi) / 2;
+			sort(array, lo, mid);
 			sort(array, mid + 1, hi);
+			merge(array, lo, mid, hi);
+			
 		}
 	}
 	
-	private int partition(int[][] array, int lo, int hi) {
-		int pivot = hi;
-		int i = lo - 1;
+	private void merge(int[][] array, int lo, int mid, int hi) {
+		int left = lo;
+		int right = mid + 1;
 		
-		for (int j = lo; j < hi; j++) {
-			if ((array[j][1] + array[j][1]) <= (array[pivot][0] + array[pivot][1])) {
-				i = i + 1;
-				swap(array, i, j);
+		for (int i = lo; i <= hi; i++) {
+			aux[i] = array[i];
+		}
+		
+		for (int i = lo; i <= hi; i++) {
+			if (left > mid) {
+				array[i] = aux[right++];
+			} else if (right > hi) {
+				array[i] = aux[left++];
+			} else if ((aux[left][0] + (hamstersNumber * aux[left][1])) < (aux[right][0] + (hamstersNumber * aux[right][1]))) {
+				array[i] = aux[left++];
+			} else {
+				array[i] = aux[right++];
 			}
 		}
-		swap(array, i + 1, hi);
-		return i + 1;
 	}
 	
 	private void swap(int[][] array, int i, int j) {
