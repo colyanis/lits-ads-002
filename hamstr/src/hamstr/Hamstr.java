4cb852bcc2fa8c1bf0ad837ac541cee66824dd30
M <M@M-PC>
2015-11-08 20:15:08 +0200

    Changed int to BigInteger.

--- hamstr/src/hamstr/Hamstr.java
+++ hamstr/src/hamstr/Hamstr.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class Hamstr {
 	private int[][] aux;
 	private int hamstersNumber;
-	private int dailyStock;
+	private BigInteger dailyStock;
 	
 	public static void main(String[] args) throws IOException {
 		String inputFile = "hamstr.in";
@@ -31,7 +32,8 @@ public class Hamstr {
 		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferReader = new BufferedReader(fileReader);
 		
-		dailyStock = Integer.parseInt(bufferReader.readLine());
+		String dailyStockString = bufferReader.readLine();
+		dailyStock = new BigInteger(dailyStockString);
 		hamstersNumber = Integer.parseInt(bufferReader.readLine());
 		aux = new int[hamstersNumber][2];
 		
@@ -44,6 +46,7 @@ public class Hamstr {
 		int i = 0;
 		while ((currentLine = bufferReader.readLine()) != null) {
 			String[] singleHamsterFoodString = currentLine.split(" ");
+			System.out.println(i);
 			foodContainer[i][0] = Integer.parseInt(singleHamsterFoodString[0]);
 			foodContainer[i++][1] = Integer.parseInt(singleHamsterFoodString[1]);
 		}
@@ -53,22 +56,25 @@ public class Hamstr {
 		writeToFile(outputFile, Integer.toString(affordedHamsters));
 	}
 	
-	private int calculateConsumedFood(int[][] foodContainer) {
-		System.out.println("Daily stock: " + dailyStock);
-		System.out.println("Number of hamsters: " + hamstersNumber);
-		
-		
-		for (int i = hamstersNumber; i > 0; i--) {
+	private int calculateConsumedFood(int[][] foodContainer) {		
+		for (int i = 1; i < hamstersNumber; i++) {
+			System.out.println("Entering:");
 			sort(i - 1, foodContainer);
-			int affordableFood = 0;
+			System.out.println("Exit:");
+			BigInteger affordableFood = BigInteger.valueOf(0);
 			for (int hamsterInProgress = 1; hamsterInProgress <= i; hamsterInProgress++) {
-				affordableFood = affordableFood + foodContainer[hamsterInProgress - 1][0] + (i - 1) * foodContainer[hamsterInProgress - 1][1];
+				BigInteger self = BigInteger.valueOf(foodContainer[hamsterInProgress - 1][0]);
+				BigInteger compatitors = BigInteger.valueOf(i - 1);
+				BigInteger greed = BigInteger.valueOf(foodContainer[hamsterInProgress - 1][1]).multiply(compatitors);
+				affordableFood = affordableFood.add(self);
+				affordableFood = affordableFood.add(greed);
+
 				System.out.println("Hamster personal: " + foodContainer[hamsterInProgress - 1][0] + 
 						" -- greedy: " + foodContainer[hamsterInProgress - 1][1]);
 			}
-			if (affordableFood <= dailyStock) {
+			if (affordableFood.compareTo(dailyStock) > 0) {
 				System.out.println("Affordable food: " + affordableFood);
-				return i;
+				return i - 1;
 			}
 		}
 		return 0;
@@ -80,25 +86,32 @@ public class Hamstr {
 	
 	private void sort(int h, int[][] array, int lo, int hi) {
 		if (lo < hi) {
-			int q = partition(h, array, lo, hi);
-			sort(h, array, lo, q - 1);
-			sort(h, array, q + 1, hi);
+			int mid = (lo + hi) / 2;
+			sort(h, array, lo, mid);
+			sort(h, array, mid + 1, hi);
+			merge(h, array, lo, mid, hi);
 		}
 	}
 	
-	private int partition(int h, int[][] array, int lo, int hi) {
-		int pivot = array[hi][0] + h * array[hi][1];
-		int i = lo - 1;
+	private void merge(int h, int[][] array, int lo, int mid, int hi) {
+		int left = lo;
+		int right = mid + 1;
 		
-		for (int j = lo; j <= hi; j++) {
-			if ((array[j][0] + h * array[j][1]) < pivot) {
-				i += 1;
-				swap(array, i, j);
-			}
+		for (int i = lo; i <= hi; i++) {
+			aux[i] = array[i];
 		}
-		swap(array, i + 1, hi);
 		
-		return i + 1;
+		for (int i = lo; i <= hi; i++) {
+			if (left > mid) {
+				array[i] = aux[right++];
+			} else if (right > hi) {
+				array[i] = aux[left++];
+			} else if ((aux[left][0] + h * aux[left][1]) < (aux[right][0] + h * aux[right][1])) {
+				array[i] = aux[left++];
+			} else {
+				array[i] = aux[right++];
+			}
+		}		
 	}
 	
 	private void swap(int[][] array, int i, int j) {
